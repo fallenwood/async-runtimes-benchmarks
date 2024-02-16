d97b6ca7102acd64d553570c63c6b8cb5dd54834
fallenwood <fallenwood.y@outlook.com>
2024-02-16 19:57:16 +0800

    tmp

--- images.psm1
+++ images.psm1
@@ -8,9 +8,11 @@ function Get-TagName {
     return "cb:${tag}-$version"
 }
 
-$images = @{ Tag = "dotnet"; Dockerfile="./dotnet/Dockerfile"; Directory="./dotnet"; Command="./dotnet" },
-    @{ Tag = "python-asyncio"; Dockerfile="./python/asyncio/Dockerfile"; Directory="./python/asyncio"; Command = "python3 main.py" }
-    # @{ Tag = "python-gevent"; Dockerfile="./python/gevent/Dockerfile"; Directory="./python/geven"; Command = "python3 main.py"}
+$images = @{ Tag = "dotnet"; Dockerfile="./dotnet/Dockerfile"; Directory="./dotnet"; },
+    @{ Tag = "python-asyncio"; Dockerfile="./python/asyncio/Dockerfile"; Directory="./python/asyncio";  },
+    @{ Tag = "python-gevent"; Dockerfile="./python/gevent/Dockerfile"; Directory="./python/gevent"; },
+    @{ Tag = "nodejs"; Dockerfile="./nodejs/Dockerfile.node"; Directory="./nodejs"; },
+    @{ Tag = "go"; Dockerfile="./go/Dockerfile"; Directory="./go"; }
 
 function Get-Images {
     return $images
--- python/gevent/main.py
+++ python/gevent/main.py
@@ -1,4 +1,5 @@
 import gevent
+import sys
 
 def perform_task():
     gevent.sleep(10)
